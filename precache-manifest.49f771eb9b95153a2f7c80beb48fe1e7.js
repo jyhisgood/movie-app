6b5591d5d96401c55aa605b089d14325529ec1a5
jyhisgood <jaiemf@naver.com>
2020-04-22 20:43:48 +0900

    Updates

---
+++ precache-manifest.49f771eb9b95153a2f7c80beb48fe1e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5fbd7690bdcbe142daf8f2b37436aa",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "b70343f15cba4d389c83",
+    "url": "/movie-app/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "303a05e924baacb61ef2",
+    "url": "/movie-app/static/js/2.e7a61427.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.e7a61427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b70343f15cba4d389c83",
+    "url": "/movie-app/static/js/main.ecd4abff.chunk.js"
+  },
+  {
+    "revision": "ef13b2d46e7e451c8c9d",
+    "url": "/movie-app/static/js/runtime-main.4744ba8c.js"
+  }
+]);
